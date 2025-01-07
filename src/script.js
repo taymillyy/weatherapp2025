edf933192ca6002524a81c0ff6a05de6a66d8808
taymillyy <taymarie008@gmail.com>
2025-01-07 04:33:54 -0600

    created the api weather

--- src/script.js
+++ src/script.js
@@ -3,7 +3,21 @@ function showCity(event){
     let text = document.querySelector("#text-form");
     let city = document.querySelector("#city");
     city.innerHTML= text.value;
+    displayCity(text.value);
 }
 
 let form = document.querySelector("#form");
 form.addEventListener("submit", showCity);
+
+function displayCity (city){
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+    let apiKey = "ae7b90f5bo7b8e38ebb290dt472f4b1b";
+    axios.get(apiUrl).then(displayWeather);
+}
+
+
+function displayWeather(response){
+    let temperature = document.querySelector("#temperature");
+     temperature.innerHTML = response.data.temperature.current;
+  
+}
