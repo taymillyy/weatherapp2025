8e2affb552421969473103c3994117c49463a727
taymillyy <taymarie008@gmail.com>
2025-01-07 21:26:26 -0600

    added the current time

--- src/script.js
+++ src/script.js
@@ -1,25 +1,49 @@
 function displayWeather(response){
+    // Get and display temperature in Fahrenheit
     let temperature = document.querySelector("#temperature");
      temperature.innerHTML = Math.round(response.data.temperature.current);
      let city = document.querySelector("#city");
      city.innerHTML= response.data.city;
        
      // Get and display humidity description
-     let humidity = document.querySelector("#humidity");
-     humidity.innerHTML = response.data.temperature.humidity;
+     let humidity = document.querySelector ("#humidity");
+     humidity.innerHTML = response.data.temperature.humidity + "%";
 
 // Get and display wind speed
      let windSpeed = document.querySelector("#wind");
-     windSpeed.innerHTML = Math.round(response.data.wind.speed) + " mph";
-  
+     windSpeed.innerHTML = Math.round(response.data.wind.speed) + "km/h";
+
+   // Get and display weather description
+     let description = document.querySelector("#weather-description");
+     description.innerHTML = response.data.condition.description;
+
+     let currentTime = document.querySelector("#current-time");
+     let date = new Date(response.data.time  * 1000);
+     currentTime.innerHTML = displayDate(date);
 }
+
+
+// Function to display current date and time in the format "Day, Hours:Minutes" 
+function displayDate(date){
+let hours = date.getHours();
+let minutes = date.getMinutes();
+if (minutes < 10) { minutes = "0" + minutes; };
+if (hours < 10) { hours = "0" + hours; };
+
+let daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let day = daysOfWeek[date.getDay()];
+return `${day} ${hours} : ${minutes}`;
+}
+
+
+// Function to get weather data for a given city and display it 
 function displayCity (city){
     let apiKey = "ae7b90f5bo7b8e38ebb290dt472f4b1b";
     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
    
     axios.get(apiUrl).then(displayWeather);
 }
-
+//
 function showCity(event){
     event.preventDefault();
     let text = document.querySelector("#text-form");
